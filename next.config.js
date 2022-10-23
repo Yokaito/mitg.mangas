166b50b44620fd4a3da32ae073f3fb499ec74d17
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-10-23 12:01:54 -0400

    feat: add styled components

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   /* config options here */
   reactStrictMode: true,
   optimizeFonts: true,
-};
+  compiler: {
+    styledComponents: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
