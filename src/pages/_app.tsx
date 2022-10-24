f63889a402a46e4be829b80835e919114bf126c7
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-10-24 19:57:21 -0400

    feat: add nextjs progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,13 @@ import GlobalStyle from '@/styles/resets/createGlobalStyle'
 import theme from '@/styles/theme/theme'
 import { ThemeProvider } from 'styled-components'
 import ErrorBoundary from '@/sdk/error/ErrorBoundary'
+import NextNProgress from 'nextjs-progressbar'
 
 const MyApp: AppType = ({ Component, pageProps, router }) => {
   return (
     <ThemeProvider theme={theme}>
       <ErrorBoundary>
+        <NextNProgress height={4} color="#209cee" />
         <Component {...pageProps} key={router.route} />
       </ErrorBoundary>
       <GlobalStyle />
