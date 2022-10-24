4eeb6837bcfa981168b23d02056359aeb07314eb
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-10-24 19:52:38 -0400

    feat: input theme and global style in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppType } from 'next/app'
 import { trpc } from '@/sdk/utils/trpc'
+import GlobalStyle from '@/styles/resets/createGlobalStyle'
+import theme from '@/styles/theme/theme'
+import { ThemeProvider } from 'styled-components'
 
 const MyApp: AppType = ({ Component, pageProps, router }) => {
-  return <Component {...pageProps} key={router.route} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} key={router.route} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 export default trpc.withTRPC(MyApp)
