2b9c1ad1ca52908cc754bfe989aa52c5c8c42a2c
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-10-26 14:29:09 -0400

    feat: add react-toastfy to use toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/fonts.css'
+import 'react-toastify/dist/ReactToastify.min.css'
 import type { AppType } from 'next/app'
 import { trpc } from '@/sdk/utils/trpc'
 import GlobalStyle from '@/styles/resets/createGlobalStyle'
@@ -6,6 +7,7 @@ import theme from '@/styles/theme/theme'
 import { ThemeProvider } from 'styled-components'
 import ErrorBoundary from '@/sdk/error/ErrorBoundary'
 import NextNProgress from 'nextjs-progressbar'
+import { ToastContainer } from 'react-toastify'
 
 const MyApp: AppType = ({ Component, pageProps, router }) => {
   return (
@@ -14,6 +16,19 @@ const MyApp: AppType = ({ Component, pageProps, router }) => {
         <NextNProgress height={4} color="#209cee" />
         <Component {...pageProps} key={router.route} />
       </ErrorBoundary>
+      <ToastContainer
+        position="bottom-left"
+        autoClose={3000}
+        limit={5}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import { trpc } from '@/sdk/utils/trpc'
+import { toast } from 'react-toastify'
 
 export const Home = () => {
   const { data } = trpc.hello.greetings.useQuery({ name: 'World' })
+  const notify = () => toast.info('Wow so easy !')
 
-  return <>{JSON.stringify(data)}</>
+  return (
+    <>
+      {JSON.stringify(data)}
+      <button onClick={notify}>Toast</button>
+    </>
+  )
 }
 
 export default Home
